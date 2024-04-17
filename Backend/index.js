d5a86b0b9f70ca7a05afe6924ef18de263f02a8e
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-17 11:27:01 +0300

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import { PORT , MONGO_URL } from "./config.js";
+import booksRoute from "./routes/booksRoute.js";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/books' , booksRoute)
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ['GET' , 'POST' , 'DELETE' , 'PUT'],
+    allowedHeaders: ['Content-Type'],
+}))
+
+app.get('/' , (request,response) =>{
+    console.log();
+    return response.status(260).send("Welcome to MERN Stack App");
+});
+
+
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("App connected to database");
+
+        app.listen(PORT , () =>{
+          console.log(`The App is listening to port: ${PORT}`);
+    });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
