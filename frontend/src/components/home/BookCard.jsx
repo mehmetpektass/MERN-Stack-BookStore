2d2141e7117a2e614fa2db92b46e910b8085e661
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-19 09:52:08 +0300

    edited size of some icons

--- frontend/src/components/home/BookCard.jsx
+++ frontend/src/components/home/BookCard.jsx
@@ -5,9 +5,6 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 
-
-
-
 const BookCard = ({books}) => {
   return (
     <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
@@ -16,11 +13,11 @@ const BookCard = ({books}) => {
                 <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">{item.publishYear}</h2>
                 <h4 className="my-2 text-gray-500">{item._id}</h4>
                 <div className="flex justify-start items-cener gap-x-2">
-                    <PiBookOpenTextLight className="text-red-300 text-2xl"/>
+                    <PiBookOpenTextLight className="text-red-300 text-3xl"/>
                     <h2 className="my-1">{item.title}</h2>
                 </div>
                 <div className="flex justify-start items-cener gap-x-2">
-                    <BiUserCircle className="text-red-300 text-2xl"/>
+                    <BiUserCircle className="text-red-300 text-3xl"/>
                     <h2 className="my-1">{item.author}</h2>
                 </div>
                 <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
