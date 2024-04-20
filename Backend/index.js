88d5bad9658f8c89fe7bf984ad4a204f2eaa113c
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-20 18:48:15 +0300

    edited some text

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.get("/", (request, response) => {
   console.log();
-  return response.status(260).send("Welcome to MERN Stack App");
+  return response.status(260).send("Welcome to MERN Stack pp");
 });
 
 app.use("/books", booksRoute);
